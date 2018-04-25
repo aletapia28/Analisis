8a7474bfab1af5ad1f8d1e711214acb9d4f78862
Alejandro Tapia <aletapiab28@gmail.com>
2018-04-25 00:18:18 -0600

    Cambios

--- Controlador/src/controlador/Controlador.java
+++ Controlador/src/controlador/Controlador.java
@@ -4,20 +4,27 @@
  * and open the template in the editor.
  */
 package controlador;
+//package Grafo;
 
-/**
- *
- * @author aleta
- */
+import java.util.Random;
+
+
+
+
 
 public class Controlador {
-       
-    
-
-    
-    
+   public static int CantidadEstaciones=0; 
+   public static int AltoPista=0;
+   public static int AnchoPista =0;
+   public static int CantidadDeViajes=0;
+   public static int TiempoSimulacion=0;
+   public static int CantidadArcos=0;
+   
+   
+      
     public static void main(String[] args) {
         // TODO code application logic here
-    }
-    
+        
+       
 }
+ }   
